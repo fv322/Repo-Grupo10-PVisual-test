251a00cfe82aa81427d4837d6868d9638b89931e
fv322 <38471258@fi.unju.edu.ar>
2022-04-14 13:42:39 -0300

    Se Agrego Control de Ingreso .

--- TP2_Grupo10/src/main/java/ar/edu/unju/fi/Factorial/Principal.java
+++ TP2_Grupo10/src/main/java/ar/edu/unju/fi/Factorial/Principal.java
@@ -1,28 +1,37 @@
 package ar.edu.unju.fi.Factorial;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		
+		System.out.println("DETERMINAR EL FACTORIAL DE UN NUMERO INGRESADO.");
 		System.out.println("Ingrese numero: ");
-		Scanner entrada = new Scanner(System.in);
-		int numero = entrada.nextInt();
-
+		Scanner entrada;
+		int numero ;
 		int aux = 1;
-		if ((numero >= 0) && (numero <= 10)) {
-			for (int i = 1; i <= numero; i++) {
-				aux *= i;
+		try {
+			entrada = new Scanner(System.in);
+			numero = entrada.nextInt();
+			if ((numero >= 0) && (numero <= 10)) {
+				for (int i = 1; i <= numero; i++) {
+					aux *= i;
+				}
+
+				System.out.println("El factorial de " + numero + " es " + aux);
+			} else {
+				System.out.println(" El numero ingresado esta fuera del rango permitido");
 			}
+			entrada.close();
 
-			System.out.println("El factorial de " + numero + " es " + aux);
-		} else {
-			System.out.println(" El numero ingresado esta fuera del rango permitido");
-
+		} catch (InputMismatchException e) {
+			System.out.println("Error! Debe ingresar un valor numerico." );
 		}
-		entrada.close();
-
+		
+		System.out.println("FIN DEL PROGRAMA.");
 	}
-
 }
+		
+
+
