84b1233099cb505aa5db2915eb8889509f2ccb39
joaquincorimayo <joaquin97corimayo@gmail.com>
2022-04-15 05:27:56 -0300

    Creación y desarrollo del paquete SecFor para actividad 9.

---
+++ TP2_Grupo10/src/main/java/ar/edu/unju/fi/SecFor/Principal.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.SecFor;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// SECUENCIA DE NUMEROS UTILIZANDO LA ESTRUCTURA REPETITIVA FOR
+		
+		for (int i = 320; i >= 160; i -= 20) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
