558c01175ac5e70e6e0466ef7f5f9f0da83d5244
joaquincorimayo <joaquin97corimayo@gmail.com>
2022-04-15 05:33:31 -0300

    Creación y desarrollo del paquete SecWhile para actividad 10.

---
+++ TP2_Grupo10/src/main/java/ar/edu/unju/fi/SecWhile/Principal.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.SecWhile;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// SECUENCIA DE NUMEROS UTILIZANDO LA ESTRUCTURA REPETITIVA WHILE
+		int numero = 320;
+		while (numero >= 160) {
+			System.out.println(numero);
+			numero = numero - 20;
+		}
+		
+	}
+}
